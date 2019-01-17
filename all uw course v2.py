cfa9e2f757cc99c25f73e0d3bc2e89a845ec8f55
arctdav <32494274+arctdav@users.noreply.github.com>
2019-01-17 17:38:25 -0500

    Add comment for each section

--- all uw course v2.py
+++ all uw course v2.py
@@ -13,34 +13,49 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
+########################## Get Course Prefix ##################################
 # Get prefix from: schedule of classes for undergraduate students
 course_prefix = []
+
+# Access the course prefix website
 prefix_page = requests.get('http://www.adm.uwaterloo.ca/infocour/CIR/SA/under.html')
+
+# Parse the accessed website page
 prefix_soup = BeautifulSoup(prefix_page.text, 'html.parser')
+
+# Since all prefix are under select tag in HTML, scrap the information by the select tag.
 all_select = prefix_soup.find_all('select')
 second_select = all_select[1]
 second_select_all_option = second_select.find_all('option')
 for i in second_select_all_option:
     course_prefix.append(i.get('value'))
-##############################################################################
+
+
+##################### Record All Undergrad Course Info ################################
 # use prefix list from previous section, to get all undergraduate courses in UW
-    
+
+# Write column headers into the Excel file
 myfile = open('UWAllCourseList_v2.csv', 'w')
 f = csv.writer(myfile)
 f.writerow(['Prefix of Website','Course Code', 'Course Name', 'Course Description', 'Prereq','Antireq','Coreq'])
 
-ecount = 0;
+# ecount: the number of exceptions
+ecount = 0
+
+# Loop through all UW course website by course prefix
 for prefix in course_prefix:
     print("Prefix '"+prefix+"' starts")
+
+    # Access the current prefix course web page
     page = requests.get('http://www.ucalendar.uwaterloo.ca/1819/COURSE/course-'\
                         + prefix + '.html')
-    
     soup = BeautifulSoup(page.text, 'html.parser')
-     
-    a =  soup.find_all('table')
-    
+
+    # Get all course table tag by BeautifulSoup parsing
+    a = soup.find_all('table')
     useful_table_list = a#[4:len(a)-1]
-    
+
+    # Get and record course information by looping though the list of course table tag
     for course in useful_table_list:
         preq = 'None'
         anti = 'None'
@@ -74,6 +89,7 @@ for prefix in course_prefix:
         except:
             ecount += 1
 
+# Prints out the progress
 print('total useless data = ' + str(ecount))
 print('Work Done')
 myfile.close()
